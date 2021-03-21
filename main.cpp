0231758489a228e5412382f97242ec1cd055491c
Gerhard Saboia <gerhardsaboia@gmail.com>
2021-03-21 22:33:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+ int n1,n2,soma;
+
+  cout<<"Digite um numero:";
+  cin>>n1;
+  cout<<"Digite um numero:";
+  cin>>n2;
+  soma=n1+n2;
+  cout<<"Valor da soma:"<<soma;
+}
